47fbabf8661fd6e672ae7a1c328ab4c2fde4d232
Hasnain N. <mhussnainzardari34@gmail.com>
2025-02-15 16:53:28 +0500

    Removed Phone Number taking form

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// document.getElementById("orderForm").addEventListener("submit", function (event) {
-//     event.preventDefault();
+document.getElementById("orderForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-//     let name = document.getElementById("name").value;
-//     let email = document.getElementById("email").value;
-//     let contact = document.getElementById("contact").value;
-//     let plan = document.getElementById("plan").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let contact = document.getElementById("contact").value;
+    let plan = document.getElementById("plan").value;
 
-//     let message = `Hello, I would like to order a Netflix account.\n\n` +
-//         `📌 Name: ${name}\n📧 Email: ${email}\n📱 WhatsApp Contact: ${contact}\n` +
-//         `📺 Selected Plan: ${plan}\n\nPlease provide further details.`;
+    let message = `Hello, I would like to order a Netflix account.\n\n` +
+        `📌 Name: ${name}\n📧 Email: ${email}\n📱 WhatsApp Contact: ${contact}\n` +
+        `📺 Selected Plan: ${plan}\n\nPlease provide further details.`;
 
-//     let encodedMessage = encodeURIComponent(message);
+    let encodedMessage = encodeURIComponent(message);
 
-//     let whatsappNumber = "923053694346";
-
-
-//     let whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
+    let whatsappNumber = "923053694346";
 
 
-//     window.location.href = whatsappURL;
-// });
+    let whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodedMessage}`;
+
+
+    window.location.href = whatsappURL;
+});
