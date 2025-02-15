13ae0ca83108dfbd15ee8b6d9bb3ef538a0a30f4
Hasnain N. <mhussnainzardari34@gmail.com>
2025-02-15 16:54:44 +0500

    Changing in tthe forms

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ document.getElementById("orderForm").addEventListener("submit", function (event)
     event.preventDefault();
 
     let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let contact = document.getElementById("contact").value;
     let plan = document.getElementById("plan").value;
 
     let message = `Hello, I would like to order a Netflix account.\n\n` +
-        `📌 Name: ${name}\n📧 Email: ${email}\n📱 WhatsApp Contact: ${contact}\n` +
+        `📌 Name: ${name}\n` +
         `📺 Selected Plan: ${plan}\n\nPlease provide further details.`;
 
     let encodedMessage = encodeURIComponent(message);
